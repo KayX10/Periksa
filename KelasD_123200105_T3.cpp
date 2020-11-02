4f93dd45bafc82721c2deacfe65ebc3c7a37dc55
KayX10 <73020724+KayX10@users.noreply.github.com>
2020-11-02 21:50:34 +0900

    Update KelasD_123200105_T3.cpp

--- KelasD_123200105_T3.cpp
+++ KelasD_123200105_T3.cpp
@@ -8,8 +8,8 @@ int main()
     int ls;
     int lma;
     int bmkap;
-    int jp;
-    int bp;
+    int jpa;
+    int bpa;
     int skor;
     int rumusipk;
     int rumusls;
@@ -21,69 +21,69 @@ int main()
     cout << "Lama Studi (semester)\t\t= "; cin>>ls;
     cout << "Lama menjadi Asisten (semester)\t= "; cin>>lma;
     cout << "Banyaknya MKA Praktikum\t\t= "; cin>>bmkap;
-    cout << "Jenis publikasi ([4]internasional, [3]nasional, [2]regional, [1]upn) = "; cin>>jp;
-    cout << "Banyaknya publikasi\t\t= "; cin>>bp;
+    cout << "Jenis publikasi ([4]internasional, [3]nasional, [2]regional, [1]upn) = "; cin >> jpa;
+    cout << "Banyaknya publikasi\t\t= "; cin>>bpa;
 
     if (ipk>=3.500 && ipk<=3.625){
         rumusipk=20*1;
     }
-    if (ipk>=3.626 && ipk<=3.750){
+    else if (ipk>=3.626 && ipk<=3.750){
         rumusipk=20*2;
     }
-    if (ipk>=3.751 && ipk<=3.875){
+    else if (ipk>=3.751 && ipk<=3.875){
         rumusipk=20*3;
     }
-    if (ipk>=3.876 && ipk<=4.00){
+    else if (ipk>=3.876 && ipk<=4.00){
         rumusipk=20*4;
     }
 
     if (ls=7){
         rumusls=10*4;
     }
-    if (ls=8){
+    else if (ls=8){
         rumusls=10*3;
     }
-    if (ls=9){
+    else if (ls=9){
         rumusls=10*2;
     }
-    if (ls=10){
+    else if (ls=10){
         rumusls=10*1;
     }
 
     if (lma>=4){
         rumuslma=10*4*bmkap;
     }
-    if (lma=3){
+    else if (lma=3){
         rumuslma=10*3*bmkap;
     }
-    if (lma=2){
+    else if (lma=2){
         rumuslma=10*2*bmkap;
     }
-    if (lma=1){
+    else if (lma=1){
         rumuslma=10*1*bmkap;
     }
 
-    if (jp=4){
-        rumusjp=10*4*bp;
+    if (jpa=4){
+        rumusjp=10*4*bpa;
     }
-    if (jp=3){
-        rumusjp=10*3*bp;
+    else if (jpa=3){
+        rumusjp=10*3*bpa;
     }
-    if (jp=2){
-        rumusjp=10*2*bp;
+    else if (jpa=2){
+        rumusjp=10*2*bpa;
     }
-    if (jp=1){
-        rumusjp=10*1*bp;
+    else if (jpa=1){
+        rumusjp=10*1*bpa;
     }
 
-    jumlahskor=rumusipk+rumusls+rumuslma+rumusjp;
+    jumlahskor=(rumusipk+rumusls+rumuslma+rumusjp);
 
     cout <<"\n";
 
     if (ipk>3.5){
             if (ls<=10){
                     if (lma>0){
-                            if (bp>0){
+                            if (bpa>0){
                             cout << "Mahasiswa tersebut berhak mendapat Karya Cendikia\n";
                             cout << "Jumlah skor = " <<jumlahskor;
                             } else {
